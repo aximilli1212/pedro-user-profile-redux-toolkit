bbc5754d220fa13e7f592aa9ec6431e9465a162f
Emmanuel Sackey <aximilli1212@gmail.com>
2021-11-04 20:34:43 +0000

    Add new reducer function

--- src/features/user.js
+++ src/features/user.js
@@ -1,14 +1,18 @@
 import { createSlice} from "@reduxjs/toolkit";
 
+const initialStateValue = {name: "jimmio", age: 0, email: "" };
 export const userSlice = createSlice({
     name: "user",
-    initialState: {value:{name: "jimmio", age: 0, email: "" }},
+    initialState: {value: initialStateValue},
     reducers:{
         login: (state, action) =>{
             state.value = action.payload;
-        }
+        },
+        logout: (state, action) =>{
+            state.value = initialStateValue;
+        },
     }
 })
 
-export const {login} = userSlice.actions;
+export const {login, logout} = userSlice.actions;
 export default userSlice.reducer;
